e678a9a74f6b71d242e968d62a4a7078b0e61c61
Rio-Fujiwara <k023g0030@g.neec.ac.jp>
2024-08-05 13:06:46 +0900

    g

--- 00_01/main.cpp
+++ 00_01/main.cpp
@@ -16,6 +16,8 @@
 #include "../externals/DirectXTex/DirectXTex.h"
 #include "../externals/DirectXTex/d3dx12.h"
 #include "vector"
+#include <fstream>
+#include <sstream>
 
 
 #pragma comment(lib,"d3d12.lib")
@@ -556,7 +558,79 @@ D3D12_GPU_DESCRIPTOR_HANDLE GetGPUDescriptorHandle(ID3D12DescriptorHeap* descrip
 }
 
 
+struct ModelData {
 
+	std::vector<VertexData> vertices;
+	
+
+};
+
+ModelData LoadObjFile(const std::string& directoryPath, const std::string& filename) {
+
+	ModelData modelData;
+	std::vector<Vector4> positions;
+	std::vector<Vector3> normals;
+	std::vector<Vector2> texcoords;
+	std::string line;
+
+	std::ifstream file(directoryPath + "/" + filename);
+	assert(file.is_open());
+
+	while (std::getline(file, line)) {
+		std::string identifier;
+		std::istringstream s(line);
+		s >> identifier;
+
+		if (identifier == "v") {
+
+			Vector4 position;
+			s >> position.x >> position.y >> position.z;
+			position.w = 1.0f;
+			positions.push_back(position);
+
+		}
+		else if (identifier == "vt") {
+			Vector2 texcoord;
+			s >> texcoord.x >> texcoord.y;
+			texcoords.push_back(texcoord);
+
+		}
+		else if (identifier == "vn") {
+
+			Vector3 normal;
+			s >> normal.x >> normal.y >> normal.z;
+			normals.push_back(normal);
+
+		}
+		else if (identifier == "f") {
+
+			for (int32_t faceVertex = 0; faceVertex < 3; ++faceVertex) {
+				std::string vertexDefintion;
+				s >> vertexDefintion;
+
+				std::istringstream v(vertexDefintion);
+				uint32_t elementIndices[3];
+				for (int32_t element = 0; element < 3; ++element) {
+					std::string index;
+					std::getline(v, index, '/');
+					elementIndices[element] = std::stoi(index);
+				}
+
+				Vector4 position = positions[elementIndices[0] - 1];
+				Vector2 texcoord = texcoords[elementIndices[1] - 1];
+				Vector3 normal = normals[elementIndices[2] - 1];
+				modelData.vertices.push_back(vertex)
+
+
+			}
+
+		}
+
+
+	}
+
+
+}
 
 
 // Windowsアプリでのエントリーポイント(main関数)
