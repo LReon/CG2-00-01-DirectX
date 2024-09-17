92cea892991377793c453129c5a109435918e6c0
Rio-Fujiwara <k023g0030@g.neec.ac.jp>
2024-09-18 00:51:17 +0900

    s

--- 00_01/Object3D.PS.hlsl
+++ 00_01/Object3D.PS.hlsl
@@ -1,50 +1,47 @@
-#include "Object3d.hlsli"
+#include "object3d.hlsli"
 
 struct Material
 {
     float32_t4 color;
-    int32_t enableLighting;
+    int32_t enableLightng;
     float32_t4x4 uvTransform;
 };
 
-struct DirectionalLight
-{
-    float32_t4 color;
-    float32_t3 direction;
-    float intensity;
-};
-
-ConstantBuffer<Material> gMaterial : register(b0);
-ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
-
-Texture2D<float32_t4> gTexture : register(t0);
-
-SamplerState gSampler : register(s0);
-
-
-
-struct PixelShaderOutput
+struct PixcelShaderOutput
 {
     float32_t4 color : SV_Target0;
 };
 
-PixelShaderOutput main(VertexShaderOutput input)
+struct DirectrionaLight
 {
-    float4 transformdUV = mul(float32_t4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
-    float32_t4 textureColor = gTexture.Sample(gSampler, transformdUV.xy);
+    float32_t4 color; //!< ライトの色
+    float32_t3 direction; //!< ライトの向き
+    float intensity;
+};
+
+ConstantBuffer<Material> gMaterial : register(b0);
+Texture2D<float32_t4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+ConstantBuffer<DirectrionaLight> gDirectrionaLight : register(b1);
+
+PixcelShaderOutput main(VertexShaderOutput input)
+{
+    PixcelShaderOutput output;
     
-    PixelShaderOutput output;
+    float4 transformedUV = mul(float32_t4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
+    float32_t4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
     
-    if (gMaterial.enableLighting != 0)
+    if (gMaterial.enableLightng != 0)//Litingする場合
     {
-        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        float NdotL = dot(normalize(input.normal), -gDirectrionaLight.direction);
         float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
-        output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+        output.color = gMaterial.color * textureColor * gDirectrionaLight.color * cos * gDirectrionaLight.intensity;
     }
     else
     {
         output.color = gMaterial.color * textureColor;
     }
-
+    
+    
     return output;
 }
