7cc2cb602bb61abec99a2377855c377f0830d618
Rio-Fujiwara <k023g0030@g.neec.ac.jp>
2024-10-04 10:20:57 +0900

    j

--- 00_01/main.cpp
+++ 00_01/main.cpp
@@ -1411,7 +1411,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 			ImGui_ImplDX12_RenderDrawData(ImGui::GetDrawData(), commandList.Get());
 
 			//画面に描く処理はすべて終わり、画面に移すので、状態を遷移
-			//今回はRenderTargetからPresentにする
+			//今回はRenderTargetからPresentにする//
 			barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
 			barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_PRESENT;
 			//TransitionBarrierを張る
