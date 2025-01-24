e34a4347822fa60feb22589693be7055a48181a1
Rio-Fujiwara <k023g0030@g.neec.ac.jp>
2025-01-24 11:34:04 +0900

    k

--- 00_01/Object3D.PS.hlsl
+++ 00_01/Object3D.PS.hlsl
@@ -43,15 +43,24 @@ PixcelShaderOutput main(VertexShaderOutput input)
         float RdotE = dot(reflectLight, toEye);
         float speclarPow = pow(saturate(RdotE), gMaterial.shininess);
         
+        // HalfVectorを求めて計算-------------------------------------------
+        float32_t halfVector = normalize(-gDirectrionaLight.direction + toEye);
+        float NDotH = dot(normalize(input.normal), halfVector);
+        float specularPow = pow(saturate(NDotH), gMaterial.shininess);
+        //-------------------------------------------------------------------
+        
         float NdotL = dot(normalize(input.normal), -gDirectrionaLight.direction);
         float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
       // output.color = gMaterial.color * textureColor * gDirectrionaLight.color * cos * gDirectrionaLight.intensity;
    
+        // 拡散反射
         float32_t3 diffuse = gMaterial.color.rgb * textureColor.rgb * gDirectrionaLight.color.rgb * cos * gDirectrionaLight.intensity;
+        // 鏡面反射
         float32_t3 specular = gDirectrionaLight.color.rgb * gDirectrionaLight.intensity * speclarPow * float32_t3(1.0f, 1.0f, 1.0f);
         
+        // 拡散反射+鏡面反射
         output.color.rgb = diffuse + specular;
-        
+        // アルファ
         output.color.a = gMaterial.color.a * textureColor.a;
         
         
